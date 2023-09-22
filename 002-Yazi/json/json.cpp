982c844bdea000365d93a2ae095873696100f577
Sammy Yeung <yeungsammy2002@yahoo.com.hk>
2023-09-22 21:05:24 +0800

    -

--- 002-Yazi/json/json.cpp
+++ 002-Yazi/json/json.cpp
@@ -18,41 +18,41 @@ Json::Json(double value) : m_type(json_double) {
     m_value.m_double = value;
 }
 
-Json::Json(const char * value) : m_type(json_string) {
+Json::Json(const char* value) : m_type(json_string) {
     m_value.m_string = new std::string(value);
 }
 
-Json::Json(const std::string & value) : m_type(json_string) {
+Json::Json(const std::string& value) : m_type(json_string) {
     m_value.m_string = new std::string(value);
 }
 
 Json::Json(Type type) : m_type(type) {
     switch (m_type) {
-        case json_null:
-            break;
-        case json_bool:
-            m_value.m_bool = false;
-            break;
-        case json_int:
-            m_value.m_int = 0;
-            break;
-        case json_double:
-            m_value.m_double = 0.0;
-            break;
-        case json_string:
-            m_value.m_string = new std::string("");
-            break;
-        case json_array:
-            m_value.m_array = new std::vector<Json>();
-            break;
-        case json_object:
-            m_value.m_object = new std::map<std::string, Json>();
-        default:
-            break;
+    case json_null:
+        break;
+    case json_bool:
+        m_value.m_bool = false;
+        break;
+    case json_int:
+        m_value.m_int = 0;
+        break;
+    case json_double:
+        m_value.m_double = 0.0;
+        break;
+    case json_string:
+        m_value.m_string = new std::string("");
+        break;
+    case json_array:
+        m_value.m_array = new std::vector<Json>();
+        break;
+    case json_object:
+        m_value.m_object = new std::map<std::string, Json>();
+    default:
+        break;
     }
 }
 
-Json::Json(const Json & other) {
+Json::Json(const Json& other) {
     copy(other);
 }
 
@@ -84,7 +84,7 @@ Json::operator std::string() {
     return *(m_value.m_string);
 }
 
-Json & Json::operator[](int index) {
+Json& Json::operator[](int index) {
     if (m_type != json_array) {
         m_type = json_array;
         m_value.m_array = new std::vector<Json>();
@@ -94,14 +94,14 @@ Json & Json::operator[](int index) {
     }
     int size = (m_value.m_array)->size();
     if (index >= size) {
-        for (int i = size; i < index; ++i) {
+        for (int i = size; i <= index; ++i) {
             (m_value.m_array)->push_back(Json());
         }
     }
     return (m_value.m_array)->at(index);
 };
 
-void Json::append(const Json & other) {
+void Json::append(const Json& other) {
     if (m_type != json_array) {
         clear();
         m_type = json_array;
@@ -110,12 +110,12 @@ void Json::append(const Json & other) {
     (m_value.m_array)->push_back(other);
 };
 
-Json & Json::operator[](const char * key) {
+Json& Json::operator[](const char* key) {
     std::string name(key);
     return (*(this))[name];
 }
 
-Json & Json::operator[](const std::string & key) {
+Json& Json::operator[](const std::string& key) {
     if (m_type != json_object) {
         clear();
         m_type = json_object;
@@ -124,70 +124,72 @@ Json & Json::operator[](const std::string & key) {
     return (*(m_value.m_object))[key];
 }
 
-void Json::operator=(const Json & other) {
+void Json::operator=(const Json& other) {
     clear();
     // clear() before copy() to prevent memory leak
     copy(other);
 }
 
-void Json::copy(const Json & other) {
+void Json::copy(const Json& other) {
     m_type = other.m_type;
     switch (m_type) {
-        case json_null:
-            break;
-        case json_bool:
-            m_value.m_bool = other.m_value.m_bool;
-            break;
-        case json_int:
-            m_value.m_int = other.m_value.m_int;
-            break;
-        case json_double:
-            m_value.m_double = other.m_value.m_double;
-            break;
-        case json_string:
-            m_value.m_string = other.m_value.m_string;
-            break;
-        case json_array:
-            m_value.m_array = other.m_value.m_array;
-            break;
-        case json_object:
-            m_value.m_object = other.m_value.m_object;
-        default:
-            break;
+    case json_null:
+        break;
+    case json_bool:
+        m_value.m_bool = other.m_value.m_bool;
+        break;
+    case json_int:
+        m_value.m_int = other.m_value.m_int;
+        break;
+    case json_double:
+        m_value.m_double = other.m_value.m_double;
+        break;
+    case json_string:
+        m_value.m_string = other.m_value.m_string;
+        break;
+    case json_array:
+        m_value.m_array = other.m_value.m_array;
+        break;
+    case json_object:
+        m_value.m_object = other.m_value.m_object;
+    default:
+        break;
     }
 }
 
 void Json::clear() {
     switch (m_type) {
-        case json_null:
-            break;
-        case json_bool:
-            m_value.m_bool = false;
-            break;
-        case json_int:
-            m_value.m_int = 0;
-        case json_double:
-            m_value.m_double = 0.0;
-        case json_string: {
-            delete m_value.m_string;
-            break;
+    case json_null:
+        break;
+    case json_bool:
+        m_value.m_bool = false;
+        break;
+    case json_int:
+        m_value.m_int = 0;
+        break;
+    case json_double:
+        m_value.m_double = 0.0;
+        break;
+    case json_string: {
+        delete m_value.m_string;
+        break;
+    }
+    case json_array: {
+        for (auto it = (m_value.m_array)->begin(); it != (m_value.m_array)->end(); ++it) {
+            it->clear();
         }
-        case json_array: {
-            for (auto it = (m_value.m_array)->begin(); it != (m_value.m_array)->end(); ++it) {
-                it->clear();
-            }
-            delete m_value.m_array;
-            break;
+        delete m_value.m_array;
+        break;
+    }
+    case json_object: {
+        for (auto it = (m_value.m_object)->begin(); it != (m_value.m_object)->end(); ++it) {
+            (it->second).clear();
         }
-        case json_object: {
-            for (auto it = (m_value.m_object)->begin(); it != (m_value.m_object)->end(); ++it) {
-                (it->second).clear();
-            }
-            delete m_value.m_object;
-            break;
-        }
-        default:
-            break;
+        delete m_value.m_object;
+        break;
+    }
+    default:
+        break;
     }
     m_type = json_null;
 };
@@ -195,45 +197,50 @@ void Json::clear() {
 std::string Json::str() const {
     std::stringstream ss;
     switch (m_type) {
-        case json_null:
-            ss << "null";
-            break;
-        case json_bool:
-            ss << m_value.m_bool;
-            break;
-        case json_int:
-            ss << m_value.m_int;
-            break;
-        case json_double:
-            ss << m_value.m_double;
-            break;
-        case json_string:
-            ss << '\"' << *(m_value.m_string) << '\"';
-            break;
-        case json_array: {
-            ss << '[';
-            for (auto it = (m_value.m_array)->begin(); it != (m_value.m_array)->end(); ++it) {
-                if (it != (m_value.m_array)->begin()) {
-                    ss << ',';
-                }
-                ss << it->str();
-            }
-            ss << ']';
-            break;
+    case json_null:
+        ss << "null";
+        break;
+    case json_bool:
+        if (m_value.m_bool) {
+            ss << "true";
         }
-        case json_object: {
-            ss << '{';
-            for (auto it = (m_value.m_object)->begin(); it != (m_value.m_object)->end(); ++it) {
-                if (it != (m_value.m_object)->begin()) {
-                    ss << ',';
-                }
-                ss << '\"' << it->first << '\"' << ':' << it->second.str();
-            }
-            ss << '}';
-            break;
+        else {
+            ss << "false";
         }
-        default:
-            break;
+        break;
+    case json_int:
+        ss << m_value.m_int;
+        break;
+    case json_double:
+        ss << m_value.m_double;
+        break;
+    case json_string:
+        ss << '\"' << *(m_value.m_string) << '\"';
+        break;
+    case json_array: {
+        ss << '[';
+        for (auto it = (m_value.m_array)->begin(); it != (m_value.m_array)->end(); ++it) {
+            if (it != (m_value.m_array)->begin()) {
+                ss << ',';
+            }
+            ss << it->str();
+        }
+        ss << ']';
+        break;
+    }
+    case json_object: {
+        ss << '{';
+        for (auto it = (m_value.m_object)->begin(); it != (m_value.m_object)->end(); ++it) {
+            if (it != (m_value.m_object)->begin()) {
+                ss << ',';
+            }
+            ss << '\"' << it->first << '\"' << ':' << it->second.str();
+        }
+        ss << '}';
+        break;
+    }
+    default:
+        break;
     }
     return ss.str();
 }
--- 002-Yazi/json/main.cpp
+++ 002-Yazi/json/main.cpp
@@ -5,32 +5,32 @@
 using namespace demo::json;
 
 int main() {
-//    std::cout << "demo json" << std::endl;
+    //    std::cout << "demo json" << std::endl;
 
-//    Json v1;
-//    Json v2 = true;
-//    Json v3 = 123;
-//    Json v4 = 1.23;
-//    Json v5 = "hello world";
+    //    Json v1;
+    //    Json v2 = true;
+    //    Json v3 = 123;
+    //    Json v4 = 1.23;
+    //    Json v5 = "hello world";
 
-//    bool b = v2;
-//    int i = v3;
-//    double f = v4;
-//    const std::string & s = v5;
+    //    bool b = v2;
+    //    int i = v3;
+    //    double f = v4;
+    //    const std::string & s = v5;
 
-//    Json arr;
-//    arr[0] = true;
-//    arr[1] = 123;
-//
-//    arr.append(1.23);
-//    arr.append("hello world");
-//
-//    std::cout << arr.str() << std::endl;
-//
-//    bool b = arr[0];
-//    int i = arr[1];
-//    double f = arr[2];
-//    const std::string & s = arr[3];
+    //    Json arr;
+    //    arr[0] = true;
+    //    arr[1] = 123;
+    //
+    //    arr.append(1.23);
+    //    arr.append("hello world");
+    //
+    //    std::cout << arr.str() << std::endl;
+    //
+    //    bool b = arr[0];
+    //    int i = arr[1];
+    //    double f = arr[2];
+    //    const std::string & s = arr[3];
 
     Json obj;
     obj["bool"] = true;
@@ -39,6 +39,8 @@ int main() {
     obj["str"] = "hello world";
 
     std::cout << obj.str() << std::endl;
+
+    // 1:01:46
 
     return 0;
 }
